d78ae2410da6f7a7b6f46d13d00f03fc1a6a707a
federicomaffei <federico.maffei@gmail.com>
2014-11-05 14:31:04 +0000

    added event model.

--- spec/features/event_feature_spec.rb
+++ spec/features/event_feature_spec.rb
@@ -6,6 +6,12 @@ describe 'event' do
 			visit '/events'
 			expect(page).to have_content 'No Events Yet Please Help'
 		end
+
+		it 'with one event' do
+			Event.create(title: 'test event', description: 'test description', 
+				start_date: Time.new(2014, 11, 05, 12, 0, 0, "+00:00"), end_date: Time.new(2014, 11, 07, 14, 0, 0, "+00:00"))
+			visit '/events'
+			expect(page).to have_content 'test event - test description goes from 2014-11-05 12:00:00 UTC to 2014-11-07 14:00:00 UTC'
+		end
 	end
 end
-
