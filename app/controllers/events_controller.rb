c6b0c0bb93d9471f78b46d5ae070dd2a5abe1e55
Painted <will.allen@paintedltd.co.uk>
2014-11-05 15:24:51 +0000

    set up form for new events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,15 @@ class EventsController < ApplicationController
 	def index
 		@events = Event.all
 	end
+
+	def new
+		@event = Event.new
+	end
+
+	def create
+		@event = Event.new(params.require(:event).permit(:title, :description, :start_date, :end_date))
+		@event.save
+		redirect_to '/events'
+	end
+
 end
--- spec/features/event_feature_spec.rb
+++ spec/features/event_feature_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
+def select_date_and_time(date, options = {})
+  field = options[:from]
+  select date.strftime('%Y'), :from => "#{field}_1i" #year
+  select date.strftime('%B'), :from => "#{field}_2i" #month
+  select date.strftime('%d'), :from => "#{field}_3i" #day 
+  select date.strftime('%H'), :from => "#{field}_4i" #hour
+  select date.strftime('%M'), :from => "#{field}_5i" #minute
+end
+
 describe 'event' do 
 	context 'initial setup' do 
 		it 'without events' do 
@@ -9,9 +18,21 @@ describe 'event' do
 
 		it 'with one event' do
 			Event.create(title: 'test event', description: 'test description', 
-				start_date: Time.new(2014, 11, 05, 12, 0, 0, "+00:00"), end_date: Time.new(2014, 11, 07, 14, 0, 0, "+00:00"))
+				start_date: Time.new(2014, 11, 5, 12, 0, 0, "+00:00"), end_date: Time.new(2014, 11, 7, 14, 0, 0, "+00:00"))
 			visit '/events'
 			expect(page).to have_content 'test event - test description goes from 2014-11-05 12:00:00 UTC to 2014-11-07 14:00:00 UTC'
+		end
+	end
+
+	context 'addition by form' do 
+		it 'includes title, description, start_date and end_date' do 
+			visit 'events/new' 
+			fill_in 'Title', with: 'Event 1'
+			fill_in 'Description', with: 'Really Cool Event'
+			select_date_and_time(Time.new(2014, 11, 10, 12, 0 ), from:'event_start_date')
+			select_date_and_time(Time.new(2015, 12, 11, 14, 1 ), from:'event_end_date')
+			click_button 'Submit'
+			expect(page).to have_content 'Event 1 - Really Cool Event goes from 2014-11-10 12:00:00 UTC to 2015-12-11 14:01:00 UTC'
 		end
 	end
 end
