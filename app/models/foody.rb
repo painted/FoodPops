ece4c437adfb96ad85f61273109d0fd15827c4dc
federicomaffei <federico.maffei@gmail.com>
2014-11-06 14:24:16 +0000

    added username field to foody sign up.

--- app/models/foody.rb
+++ app/models/foody.rb
@@ -3,6 +3,5 @@ class Foody < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable
-
   has_many :events
 end
--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ require "sprockets/railtie"
 Bundler.require(*Rails.groups)
 
 module FoodPops
-  class Application < Rails::Application
+	class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
