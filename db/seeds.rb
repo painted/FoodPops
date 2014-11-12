42c341993bd63c7417f29eb86dd5d0c5a6c9199e
federicomaffei <federico.maffei@gmail.com>
2014-11-12 16:37:03 +0000

    added London seeding and fixed no events link.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'open-uri'
 
-content = open("https://www.eventbriteapi.com/v3/events/search/?categories=110&token=#{Rails.application.secrets.eventbrite_token}").read
+content = open("https://www.eventbriteapi.com/v3/events/search/?venue.city=London&categories=110&token=#{Rails.application.secrets.eventbrite_token}").read
 
 hash = JSON.parse content
 
-eventbrite = Foody.create email: 's@s.com', password: '12345678', password_confirmation: '12345678', username: 'EventBrite'
+eventbrite = Foody.first_or_create email: 'eventbrite@test.com', password: '12345678', password_confirmation: '12345678', username: 'EventBrite'
 
-hash["events"][1..10].each do |event|
+hash["events"][1..50].each do |event|
 	title = event['name']['text']
 	description = event['description']['text']
 	start_date = DateTime.strptime event['start']['utc']
@@ -22,4 +22,5 @@ hash["events"][1..10].each do |event|
 	city = event['venue']['address']['city']
 	postcode = event['venue']['address']['postal_code']
 	eventbrite.events.create(title: title, description: description, start_date: start_date, end_date: end_date, address: address, city: city, postcode: postcode)
+	sleep 0.25
 end
