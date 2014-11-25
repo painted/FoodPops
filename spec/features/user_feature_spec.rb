cf5a68da5cfaa090a36d9c618b5955ef0972523f
Painted <will.allen@paintedltd.co.uk>
2014-11-25 10:06:25 +0000

    added the ability to change your avatar in the edit registration page

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -46,4 +46,24 @@ describe 'Users' do
 			expect(page).to have_css 'img.uploaded-avatar'
 		end
 	end
+
+	context 'account editing' do 
+
+		before do 
+			visit new_foody_registration_path
+			fill_in 'Email', with: 's@s.com'
+			fill_in 'Password', with: '12345678'
+			fill_in 'Password confirmation', with: '12345678'
+			attach_file 'Avatar', Rails.root.join('spec/images/paintedicon.png')
+			click_button 'Sign up' 
+			click_link 'Edit Account'
+		end
+
+		it 'can change the avatar image' do 
+			attach_file 'Avatar', Rails.root.join('spec/images/paintedicon.png')
+			fill_in 'Current password', with: '12345678'
+			click_button 'Update'
+			expect(page).to have_content 'Your account has been updated successfully.'
+		end
+	end
 end
