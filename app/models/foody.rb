873d7ec1d1562403f4b6d30389e89467056f16f7
Painted <will.allen@paintedltd.co.uk>
2014-11-25 13:29:11 +0000

    added an attribute accessor for the avatar to fix problem with Paperclip, S3 and Heroku

--- app/models/foody.rb
+++ app/models/foody.rb
@@ -4,6 +4,7 @@ class Foody < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable
   has_many :events
+  attr_accessor :avatar
   has_attached_file :avatar,
 	:default_url => "/images/original/missing.png",
 	styles: { thumb: '300X300' },
