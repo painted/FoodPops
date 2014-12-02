cc17c0aaa9a6e15ae62d259aa6ff1a8e406964cf
Painted <will.allen@paintedltd.co.uk>
2014-12-02 21:12:38 +0000

    refactored the reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,9 +9,9 @@ class ReviewsController < ApplicationController
 		find_event
 		@review = @event.reviews.new params[:review].permit(:thoughts, :rating)
 		@user_id = current_foody.id 
-		if foody_review_today.present?
+		if foody_review_today?
 			flash[:notice] = 'Unfortunately, you have met your daily limit of reviews for this event.'
-		elsif @event.reviews.where("foody_id = ? AND created_at > ?", @user_id, 31.days.ago).count == 5
+		elsif foody_reviews_this_month == 5
 			flash[:notice] = 'Unfortunately, you have met your monthly limit of reviews for this event.'
 		else 
 			@review.foody = current_foody
@@ -27,8 +27,12 @@ class ReviewsController < ApplicationController
 		@event = Event.find params[:event_id]
 	end
 
-	def foody_review_today
-		@event.reviews.where("foody_id = ? AND created_at > ?", @user_id, 1.day.ago)
+	def foody_review_today?
+		@event.reviews.where("foody_id = ? AND created_at > ?", @user_id, 1.day.ago).present?
+	end
+
+	def foody_reviews_this_month
+		@event.reviews.where("foody_id = ? AND created_at > ?", @user_id, 31.days.ago).count
 	end
 
 end
