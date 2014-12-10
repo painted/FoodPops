3f89e99cd0b185e05d77018a96391141972015fa
Painted <will.allen@paintedltd.co.uk>
2014-12-10 13:20:55 +0000

    editing tests to automatically choose the right event id

--- spec/features/foody_photo_feature_spec.rb
+++ spec/features/foody_photo_feature_spec.rb
@@ -3,15 +3,19 @@ require 'rails_helper'
 describe 'photos' do 
 	context 'foodies logged in' do
 
-		before do 
-			@bob = Foody.create email: 's@s.com', password: '12345678', password_confirmation: '12345678', username: 'bob'
-			@bob.events.create(title: 'test event', description: 'test description', 
-				start_date: Time.new(2014, 11, 5, 12, 0, 0, "+00:00"), end_date: Time.new(2014, 11, 7, 14, 0, 0, "+00:00"))
-			login_as @bob
-		end
+		let!(:bob) {Foody.create email: 'b@b.com', password: '12345678', password_confirmation: '12345678', username: 'bob'}
+		let!(:event){bob.events.create(title: 'test event', description: 'test description', 
+			start_date: Time.new(2014, 11, 5, 12, 0, 0, "+00:00"), end_date: Time.new(2014, 11, 7, 14, 0, 0, "+00:00"))}
+		# before do 
+		# 	@bob = Foody.create email: 'b@b.com', password: '12345678', password_confirmation: '12345678', username: 'bob'
+		# 	@bob.events.create(title: 'test event', description: 'test description', 
+		# 		start_date: Time.new(2014, 11, 5, 12, 0, 0, "+00:00"), end_date: Time.new(2014, 11, 7, 14, 0, 0, "+00:00"))
+		# 	login_as @bob
+		# end
 
 		it 'can add a photo to an event' do
-			visit '/events/7'
+			login_as bob
+			visit "/events/#{event.id}"
 			click_link 'Add a photo'
 			fill_in 'Caption', with: 'Test Caption'
 			attach_file 'Photo', Rails.root.join('spec/images/FoodTrucks.jpg')
--- spec/features/reviews_edit_feature_spec.rb
+++ spec/features/reviews_edit_feature_spec.rb
@@ -1,16 +1,14 @@
 require 'rails_helper'
 
 context 'logged out' do
-
-    before do 
-        @bob = Foody.create(email: 's@s.com', password: '12345678', password_confirmation: '12345678', username: 'bob')
-        @bob.events.create(title: 'test event', description: 'test description', 
-            start_date: Time.new(2014, 11, 5, 12, 0, 0, "+00:00"), end_date: Time.new(2014, 11, 7, 14, 0, 0, "+00:00"))
-        login_as @bob
-    end
+ 
+    let!(:bob) {Foody.create email: 'b@b.com', password: '12345678', password_confirmation: '12345678', username: 'bob'}
+    let!(:event){bob.events.create(title: 'test event', description: 'test description', 
+        start_date: Time.new(2014, 11, 5, 12, 0, 0, "+00:00"), end_date: Time.new(2014, 11, 7, 14, 0, 0, "+00:00"))}
 
     it 'cannot Edit Reviews' do
-        visit '/events/1'
+        login_as bob
+        visit "/events/#{event.id}"
         leave_review('Amazing', 6)
         logout(:bob)
         expect(page).to have_content 'test event'
